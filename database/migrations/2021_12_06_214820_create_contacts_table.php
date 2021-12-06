25a545bead3095c52632a14acc8365d0f9fec976
praveensudharsanan <71019578+praveensudharsanan@users.noreply.github.com>
2021-12-06 17:36:11 -0500

    corrected migration file

--- database/migrations/2021_12_06_214820_create_contacts_table.php
+++ database/migrations/2021_12_06_214820_create_contacts_table.php
@@ -14,24 +14,24 @@ class CreateContactsTable extends Migration
     public function up()
     {
         Schema::create('contacts', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::create('contact', function (Blueprint $table) {
             $table->integer('id')->autoIncrement();
             $table->string('name',20);
             $table->string('email',50);
             $table->string('subject',60);
             $table->string('message',150);
         });
+    }
+
+    /**
+     * Reverse the migrations.
+
+     * @return void
+     */
+
+
+
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
     }
 }
