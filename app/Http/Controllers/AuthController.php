7b78de912d9cda5ac8060195348b684e36952e82
Saloni Yadav <saloniy97@gmail.com>
2021-12-10 20:29:02 -0500

    Corrected main file

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -158,7 +158,7 @@ class AuthController extends Controller
             $data = User::all()->where('email', '=', session()->get('username'))->first();
             $data->update($updatedData);
             $data->save();
-            session()->put('username', $updatedData[0]->email);
+            session()->put('username', $request->input('email'));
             return redirect("/profile");
         }
     }
--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -14,7 +14,7 @@
     <!--header content -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Belus Telecom</a>
+            <a class="navbar-brand" href="/">Belus Telecom</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
