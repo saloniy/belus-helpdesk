7714516c3f1f0118457b9e326d67ed72bb936f90
Saloni Yadav <saloniy97@gmail.com>
2021-12-10 20:40:13 -0500

    Corrected main file

--- resources/views/auth/myprofile.blade.php
+++ resources/views/auth/myprofile.blade.php
@@ -61,15 +61,6 @@
                 <hr>
                 <div class="row">
                     <div class="col-sm-3">
-                        <h6 class="mb-0">Mobile</h6>
-                    </div>
-                    <div class="col-sm-9 text-secondary">
-                        {{$d->contact}}
-                    </div>
-                </div>
-                <hr>
-                <div class="row">
-                    <div class="col-sm-3">
                         <h6 class="mb-0">Address</h6>
                     </div>
                     <div class="col-sm-9 text-secondary">
